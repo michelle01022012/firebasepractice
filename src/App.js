281a37dadf6d8bb75336512d53a96322639d4131
Michelle Anderson <manderson0102@yahoo.com>
2026-04-25 08:27:38 -0400

    commit user changes

--- src/App.js
+++ src/App.js
@@ -1,95 +1,106 @@
-import React from 'react';
-import './App.css';
-import { auth, db } from './firebase/init';
-import { collection, addDoc, getDocs, getDoc, doc, query, where } from 'firebase/firestore';
-import { 
-  createUserWithEmailAndPassword, 
+import React from "react";
+import "./App.css";
+import { auth, db } from "./firebase/init";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
-  onAuthStateChanged
-} from 'firebase/auth';
-function App() {
-   const [user, setUser] = React.useState({});
-   const [loading, setLoading] = React.useState(true);
+  onAuthStateChanged,
+} from "firebase/auth";
 
-   function createPost() {
+function App() {
+  const [user, setUser] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser.toJSON());
+      } else {
+        setUser(null);
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  function createPost() {
+    if (!user) {
+      alert("Please log in first");
+      return;
+    }
     const post = {
       title: "Finish Firebase Section",
       description: "Do Front End Simplified",
       uid: user.uid,
-      };
+    };
     addDoc(collection(db, "posts"), post);
-    }
-
-    async function getAllPosts() {
-      const { docs } = await getDocs(collection(db, "posts"));
-      const posts = docs.map(elem => ({...elem.data(), id: elem.id}));
-      console.log(posts);
-    }
-      
-    async function getPostById() {
-      const hardcodedId = "3fCvn7u3QtcGKj06F0B8";
-      const postRef = doc(db, "posts", hardcodedId);
-      const postSnap = await getDoc(postRef);
-      const post = postSnap.data();
-      console.log(post);
-    }
-
-    async function getPostByUid() {
-      const postCollectionRef = await query(
-        collection(db, "posts"),
-        where("uid", "==", user.uid)
-      )
-    }
-    
-   React.useEffect(() => {
-  onAuthStateChanged(auth, (user) => {
-    setLoading(false);
-    console.log(user);
-
-    if (user) {
-      setUser(user);
-    } else {
-      setUser({});
-    }
-  });
-}, []);
-   function register() {
-    createUserWithEmailAndPassword(auth, 'myemail@email.com', 'test123')
-    .then((user) => {
-      console.log(user);
-    })
-      .catch((error) => {
-        console.log(error);
-    })
   }
 
+  async function getAllPosts() {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    const posts = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    console.log(posts);
+  }
+
+  async function getPostById() {
+    const hardcodedId = "3fCvn7u3QtcGKj06F0B8";
+    const postRef = doc(db, "posts", hardcodedId);
+    const postSnap = await getDoc(postRef);
+    const post = postSnap.data();
+    console.log(post);
+  }
+
+  function register() {
+    createUserWithEmailAndPassword(auth, "myemail@email.com", "test123")
+      .then((userCredential) => {
+        console.log(userCredential.user.toJSON());
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  console.log({ user });
+
   function login() {
-    signInWithEmailAndPassword(auth, 'myemail@email.com', 'test123')
-    .then(({ user }) => {
-      console.log(user);
-      setUser(user);
-    }) 
-     .catch((error) => {
-      console.log(error.message);
-    })
+    signInWithEmailAndPassword(auth, "myemail@email.com", "test123")
+      .then(({ user }) => {
+        console.log(user.toJSON());
+        setUser(user.toJSON());
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 
   function logout() {
-    signOut(auth)
-    setUser({});
+    signOut(auth);
+    setUser(null);
   }
 
   return (
     <div className="App">
-       <button onClick={register}>Register</button>
-       <button onClick={login}>Login</button>
-       <button onClick={logout}>Logout</button>  
-       {loading ? 'Loading...' : user?.email}
-       <button onClick={createPost}>Create Post</button>
-       <button onClick={getAllPosts}>Get All Posts</button>
-       <button onClick={getPostById}>Get Post By Id</button>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? "Loading..." : user?.email}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By Id</button>
     </div>
   );
 }
+
 export default App;
